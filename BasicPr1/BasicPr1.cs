98f9c87bb8bf7fe47dedb82fe29cf7b3cc18c6db
JonnyTheCat <stels80137@gmail.com>
2019-02-14 17:45:23 +1000

    Created FirstPr1.cs и .exe
    Prints the first 100 numbers of the sequence (2, -3, 4, -5, 6, -7, 8...) to the command line // Выводит первые 100 чисел последовательности (2, -3, 4, -5, 6, -7, 8...) в командную строку

---
+++ BasicPr1/BasicPr1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToCSharp
+{
+
+    class HelloCSharp
+    {
+        
+        static void Main(string[] args)
+        {
+            int numSeq = 1;
+            for (int i = 0; i < 100; i++)
+            {
+                numSeq++;
+                if((numSeq % 2) == 0)
+                {
+                    Console.WriteLine(numSeq);
+                }
+                else
+                {
+                    Console.WriteLine(numSeq * (-1));
+                }
+            }
+            
+        }
+    }
+}
